a1eb455c8394b6f89a460a691f05f939782fba2e
dixitaayush8 <dixitaayush8@gmail.com>
2017-12-10 16:09:44 -0800

    Updated authentications

--- Top 25 Spotify Search.py
+++ Top 25 Spotify Search.py
@@ -1,7 +1,14 @@
-import spotipy
 import numpy as np
 import pandas as pd
-sp = spotipy.Spotify()
+import spotipy
+import spotipy.oauth2 as oauth2
+
+credentials = oauth2.SpotifyClientCredentials(
+        client_id='CLIENT_ID',
+        client_secret='SECRET')
+
+token = credentials.get_access_token()
+sp = spotipy.Spotify(auth=token)
 
 def getUserInput():
     someIn = str(raw_input('Search for anything on Spotify: '))
@@ -13,7 +20,7 @@ def playSearch():
         name = []
         artists = []
         r = getUserInput()
-        results = sp.search(q=r, limit=25) #searches for the string that the user inputs
+        results = sp.search(q=r, limit=40) #searches for the string that the user inputs
         if(r.upper() == 'NO'):
             print('Enjoy your day')
             return False
@@ -23,7 +30,7 @@ def playSearch():
             artists.append(t['artists'][0]['name'])
         arrays = [np.array(name),np.array(artists)] #uses Numpy package to construct a MultiIndex of the song name and artist name
         ser = pd.Series(pop,index=arrays) #uses Pandas package to construct a Series, with the index being arrays
-        serOrder = pd.Series.order(ser,ascending=False) #creates an ordered series based on the populariy
+        serOrder = pd.Series.sort_values(ser,ascending=False) #creates an ordered series based on the populariy
         print serOrder
 
 playSearch()
